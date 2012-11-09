845f912d95c5706290da5218ed5fcd193e25ea3a
mitchellsundt <mitchellsundt@gmail.com>
2012-11-09 12:24:54 -0800

    Rename base form directory to 'default' Update copy scripts to be more generic

--- form-files/copyMnt.bat
+++ form-files/copyMnt.bat
@@ -1,12 +1,6 @@
-rmdir /s /q F:\odk\js\forms\collect"
-rmdir /s /q F:\odk\js\forms\datetimeTest"
-rmdir /s /q F:\odk\js\forms\example"
-rmdir /s /q F:\odk\js\forms\imnci"
-rmdir /s /q F:\odk\js\forms\json-media"
-rmdir /s /q F:\odk\js\forms\lgform"
-rmdir /s /q F:\odk\js\forms\selectImage"
+rmdir /s /q F:\odk\js\forms\"
 xcopy . "F:\odk\js\forms" /E /I /R /Y
 del "F:\odk\js\forms\copy.bat"
-del "F:\odk\js\forms\collect\js\collect.js"
-echo # %DATE% %TIME% >> "F:\odk\js\forms\collect\manifest.appcache"
+del "F:\odk\js\forms\copyMnt.bat"
+echo # %DATE% %TIME% >> "F:\odk\js\forms\default\manifest.appcache"
 # pause 'Hit enter to continue'
