a28f478ac8053ee73b8ba4386357b9cb5d4fedf4
mitchellsundt <mitchellsundt@gmail.com>
2013-04-09 11:22:49 -0700

    Regen with javascript XLSXConverter (after fixing explicit model bug).

---
+++ form-files/adbpush.bat
@@ -0,0 +1,8 @@
+rem commands to push files to the default survey app...
+adb push breathcounter /sdcard/odk/survey/forms/breathcounter
+adb push custom_appearance /sdcard/odk/survey/forms/custom_appearance
+adb push default /sdcard/odk/survey/framework/default
+adb push example /sdcard/odk/survey/forms/example
+adb push IMNCI /sdcard/odk/survey/forms/IMNCI
+adb push shared_table_a /sdcard/odk/survey/forms/shared_table_a
+adb push shared_table_b /sdcard/odk/survey/forms/shared_table_b
