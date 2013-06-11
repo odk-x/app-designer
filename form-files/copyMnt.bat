1abdeacc4c3a2d412f3f824d64c9071c3548aa08
mitchellsundt <mitchellsundt@gmail.com>
2013-06-11 08:59:42 -0700

    Update Android 2.x copy script to place files in /odk/survey/forms and /odk/survey/framework

--- form-files/copyMnt.bat
+++ form-files/copyMnt.bat
@@ -1,6 +1,11 @@
-rmdir /s /q F:\odk\js\forms\"
-xcopy . "F:\odk\js\forms" /E /I /R /Y
-del "F:\odk\js\forms\copy.bat"
-del "F:\odk\js\forms\copyMnt.bat"
-echo # %DATE% %TIME% >> "F:\odk\js\forms\default\manifest.appcache"
+rmdir /s /q F:\odk\survey\forms\"
+rmdir /s /q F:\odk\survey\framework\"
+xcopy . "F:\odk\survey\forms" /E /I /R /Y
+rmdir /s /q F:\odk\survey\forms\default\"
+mkdir F:\odk\survey\framework\
+xcopy .\default "F:\odk\survey\framework\default" /E /I /R /Y
+del "F:\odk\survey\forms\convertToLegacy.hta"
+del "F:\odk\survey\forms\copy.bat"
+del "F:\odk\survey\forms\copyMnt.bat"
+echo # %DATE% %TIME% >> "F:\odk\survey\framework\default\manifest.appcache"
 # pause 'Hit enter to continue'
