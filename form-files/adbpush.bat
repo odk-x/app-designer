7fac780557a372745add6da8689f81a056afc719
mitchellsundt <mitchellsundt@gmail.com>
2013-08-29 15:39:14 -0700

    Script now wipes the device clean of all ODK Survey files. Update script to reflect new directory structure.

--- form-files/adbpush.bat
+++ form-files/adbpush.bat
@@ -1,9 +1,5 @@
 rem commands to push files to the default survey app...
-adb push breathcounter /sdcard/odk/survey/forms/breathcounter
-adb push custom_appearance /sdcard/odk/survey/forms/custom_appearance
-adb push default /sdcard/odk/survey/framework/default
-adb push example /sdcard/odk/survey/forms/example
-adb push IMNCI /sdcard/odk/survey/forms/IMNCI
-adb push section_test /sdcard/odk/survey/forms/section_test
-adb push shared_table_a /sdcard/odk/survey/forms/shared_table_a
-adb push shared_table_b /sdcard/odk/survey/forms/shared_table_b
+adb shell rm -r /sdcard/odk/survey/
+adb shell mkdir /sdcard/odk/survey/
+adb push framework /sdcard/odk/survey/framework
+adb push tables /sdcard/odk/survey/tables
