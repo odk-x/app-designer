ffef6fb147da7e0877c66966f86cfa859778f1ac
Sam Sudar <sudar.sam@gmail.com>
2014-02-18 22:22:54 -0800

    removed ../ in path of grunttemplate js.

--- grunttemplates/table/default/js/detail.js
+++ grunttemplates/table/default/js/detail.js
@@ -8,7 +8,7 @@
 if (JSON.parse(control.getPlatformInfo()).container === 'Chrome') {
     console.log('Welcome to Tables debugging in Chrome!');
     $.ajax({
-        url: control.getFileAsUrl('../output/debug/%TABLE_ID%_data.json'),
+        url: control.getFileAsUrl('output/debug/%TABLE_ID%_data.json'),
         async: false,  // do it first
         success: function(dataObj) {
             if (dataObj === undefined || dataObj === null) {
