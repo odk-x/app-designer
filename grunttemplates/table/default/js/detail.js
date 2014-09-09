685be91faecbe61495fcacdb4f15d66824e4c220
rumanna27 <devnull@localhost>
2014-09-09 13:55:28 -0700

    Adding files back

---
+++ grunttemplates/table/default/js/detail.js
@@ -0,0 +1,26 @@
+/**
+ * The file for displaying a detail view.
+ */
+/* global $, control */
+'use strict';
+
+// Handle the case where we are debugging in chrome.
+if (JSON.parse(control.getPlatformInfo()).container === 'Chrome') {
+    console.log('Welcome to Tables debugging in Chrome!');
+    $.ajax({
+        url: control.getFileAsUrl('output/debug/%TABLE_ID%_data.json'),
+        async: false,  // do it first
+        success: function(dataObj) {
+            if (dataObj === undefined || dataObj === null) {
+                console.log('Could not load data json for table: %TABLE_ID%');
+            }
+            window.data.setBackingObject(dataObj);
+        }
+    });
+}
+ 
+function display() {
+    // Perform your modification of the HTML page here and call display() in
+    // the body of your .html file.
+}
+
