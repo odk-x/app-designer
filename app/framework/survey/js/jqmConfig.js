6df73b3bc471dcd15ba497284734abe11849c475
rumanna27 <devnull@localhost>
2014-09-09 13:51:01 -0700

    Removing all files

--- app/framework/survey/js/jqmConfig.js
+++
@@ -1,8 +0,0 @@
-/**
- * Configuration settings for jquery mobile to disable 
- * dynamic page transitions (loadings of pages via links),
- * disable page history and to disable all hashChange
- * detection or handling (do our own navigation handling).
- */
-define(['jquery'], function($){
-});
