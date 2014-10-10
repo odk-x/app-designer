48944dcfdd2130166e1878b72104ab9004f2a09b
Sam Sudar <sudar.sam@gmail.com>
2014-10-10 11:55:06 -0700

    add ability to get data and send in sms.

--- app/tables/send_sms/forms/send_sms/customPromptTypes.js
+++ app/tables/send_sms/forms/send_sms/customPromptTypes.js
@@ -1,5 +1,5 @@
-define(['promptTypes', 'jquery', 'underscore', 'prompts'],
-    function(promptTypes, $, _) {
+define(['promptTypes', 'jquery', 'underscore', 'prompts', 'database'],
+    function(promptTypes, $, _, prompts, database) {
         return {
           'send_sms': promptTypes.launch_intent.extend({
             type: 'send_sms',
@@ -7,11 +7,28 @@ define(['promptTypes', 'jquery', 'underscore', 'prompts'],
             buttonLabel: {
               'default': 'Send SMS'
             },
-            intentString: 'vnd.android-dir/mms-sms',
+            // this has access to the saved value. the "that" function here
+            // will have access to saved data. have to leave the other stuff
+            // here, which I got from a launch_intent prompt in prompts.js, to
+            // ensure that current functionality is maintained.
+            configureRenderContext: function(ctxt) {
+                var that = this;
+                var value = that.getValue();
+                that.intentParameters.extras.sms_body =
+                    'Name is : ' +
+                    that.database.getDataValue('subject_name');
+                that.renderContext.value = value;
+                that.renderContext.buttonLabel = that.buttonLabel;
+                ctxt.success();
+            },
+            intentString: 'android.intent.action.VIEW',
             intentParameters: {
+              type: 'vnd.android-dir/mms-sms',
+              uri: 'sms:3604301468',
               extras: {
-                sms_body: 'This is sent from Survey',
-                address: '3604301468'
+                sms_body: number,
+                sms_body: 'Default message'
+                //address: '3604301468'
               }
             },
             extractDataValue: function(jsonObject) {
