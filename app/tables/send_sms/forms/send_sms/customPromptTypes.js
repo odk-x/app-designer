ffa168e4dce7457cb824157bd58f978fef6d17a9
Sam Sudar <sudar.sam@gmail.com>
2014-10-29 10:11:05 -0700

    add some SMS examples.

--- app/tables/send_sms/forms/send_sms/customPromptTypes.js
+++ app/tables/send_sms/forms/send_sms/customPromptTypes.js
@@ -1,5 +1,5 @@
-define(['promptTypes', 'jquery', 'underscore', 'prompts', 'database'],
-    function(promptTypes, $, _, prompts, database) {
+define(['promptTypes', 'jquery', 'underscore', 'prompts'],
+    function(promptTypes, $, _, prompts) {
         return {
           'send_sms': promptTypes.launch_intent.extend({
             type: 'send_sms',
@@ -11,28 +11,94 @@ define(['promptTypes', 'jquery', 'underscore', 'prompts', 'database'],
             // will have access to saved data. have to leave the other stuff
             // here, which I got from a launch_intent prompt in prompts.js, to
             // ensure that current functionality is maintained.
-            configureRenderContext: function(ctxt) {
+            configureRenderContext:
+              function(ctxt) {
                 var that = this;
                 var value = that.getValue();
                 that.intentParameters.extras.sms_body =
                     'Name is : ' +
                     that.database.getDataValue('subject_name');
+                that.intentParameters.uri =
+                  'sms:' + 
+                  that.database.getDataValue('phone_number');
                 that.renderContext.value = value;
                 that.renderContext.buttonLabel = that.buttonLabel;
                 ctxt.success();
-            },
+              },
             intentString: 'android.intent.action.VIEW',
             intentParameters: {
               type: 'vnd.android-dir/mms-sms',
-              uri: 'sms:3604301468',
               extras: {
-                sms_body: number,
                 sms_body: 'Default message'
-                //address: '3604301468'
               }
             },
             extractDataValue: function(jsonObject) {
               console.log('Got returned intent from sms: ' + jsonObject);
+            }
+          }),
+          'odk_sms': promptTypes.launch_intent.extend({
+            type: 'odk_sms',
+            datatype: 'odk_sms',
+            buttonLabel: {
+              'default': 'SMS Bridge'
+            },
+            // this has access to the saved value. the "that" function here
+            // will have access to saved data. have to leave the other stuff
+            // here, which I got from a launch_intent prompt in prompts.js, to
+            // ensure that current functionality is maintained.
+            configureRenderContext:
+              function(ctxt) {
+                var that = this;
+                var value = that.getValue();
+                that.intentParameters.extras.sms_body =
+                    'Name is : ' +
+                    that.database.getDataValue('subject_name');
+                that.intentParameters.extras.phone_number =
+                    that.database.getDataValue('phone_number');
+                that.renderContext.value = value;
+                that.renderContext.buttonLabel = that.buttonLabel;
+                ctxt.success();
+              },
+            intentString: 'org.opendatakit.smsbridge.activity.SMSDispatcherActivity',
+            intentParameters: {
+              extras: {
+                require_confirmation: true,
+                delete_after_sending: false,
+                message_body: 'Message from odk sms',
+              }
+            }
+          }),
+          'odk_sms_automatic': promptTypes.launch_intent.extend({
+            type: 'odk_sms_automatic',
+            datatype: 'odk_sms_automatic',
+            buttonLabel: {
+              'default': 'SMS Bridge Auto'
+            },
+            // this has access to the saved value. the "that" function here
+            // will have access to saved data. have to leave the other stuff
+            // here, which I got from a launch_intent prompt in prompts.js, to
+            // ensure that current functionality is maintained.
+            configureRenderContext:
+              function(ctxt) {
+                var that = this;
+                var value = that.getValue();
+                that.intentParameters.extras.sms_body =
+                    'Name is: ' +
+                    that.database.getDataValue('subject_name');
+                that.intentParameters.extras.phone_number =
+                    that.database.getDataValue('phone_number');
+                that.renderContext.value = value;
+                that.renderContext.buttonLabel = that.buttonLabel;
+                ctxt.success();
+              },
+            intentString: 'org.opendatakit.smsbridge.activity.SMSDispatcherActivity',
+            intentParameters: {
+              extras: {
+                require_confirmation: false,
+                delete_after_sending: false,
+                message_body: 'Message from odk sms',
+                phone_number: '3604301468'
+              }
             }
           })
         };
