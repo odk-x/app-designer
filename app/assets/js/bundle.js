f7f13cfeee5717867aebb761021c82a0300cd0f8
Sam Sudar <sudar.sam@gmail.com>
2015-04-20 15:30:32 -0700

    remove dead comments from bundle

--- app/assets/js/bundle.js
+++ app/assets/js/bundle.js
@@ -602,21 +602,6 @@ exports.getFocalChimpIdFromUrl = function() {
 },{}],5:[function(require,module,exports){
 'use strict';
 
-// var chimpIsPresent = function chimpIsPresent(chimpId) {
-
-// };
-
-// /**
-//  * Get the default state for a chimp that nothing is known about. I.e. this
-//  * would be the state of the chimp when the follow begins.
-//  */
-// var getDefaultChimpState = function getDefaultChimpState() {
-
-// };
-
-// // Mutate chimp states
-
-
 exports.incrementTime = function(time) {
 
   var interval = 15;
