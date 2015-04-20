7d829e562ced50774aeba4fab6f169e4e4bbe6ff
Sam Sudar <sudar.sam@gmail.com>
2015-04-20 15:15:41 -0700

    remove comments

--- app/assets/js/jgiUtil.js
+++ app/assets/js/jgiUtil.js
@@ -1,20 +1,5 @@
 'use strict';
 
-// var chimpIsPresent = function chimpIsPresent(chimpId) {
-
-// };
-
-// /**
-//  * Get the default state for a chimp that nothing is known about. I.e. this
-//  * would be the state of the chimp when the follow begins.
-//  */
-// var getDefaultChimpState = function getDefaultChimpState() {
-
-// };
-
-// // Mutate chimp states
-
-
 exports.incrementTime = function(time) {
 
   var interval = 15;
