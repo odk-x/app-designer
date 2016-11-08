5e5aa3a512edc976f2d71ca393493e8d9f743ccc
Adam Rea <area@cs.washington.edu>
2016-11-07 16:43:11 -0800

    Added the structure and files needed to have a combo demo of the cold chain app and the row level access demo (.html, .js, etc)

---
+++ app/config/assets/js/demoChooser.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var currentTab = 0;
+
+function display() { 
+    updateForTab(currentTab);
+    // set up the click listeners.
+    $('#coldChainTab').on('click', function() {
+        currentTab = 0;
+        updateForTab(currentTab);
+    });
+    $('#geoWeatherTab').on('click', function() {
+        currentTab = 1;
+        updateForTab(currentTab);
+    });
+
+    $('#launch-button').on(
+            'click',
+            function() {
+                if (currentTab === 0) {
+                    odkTables.launchHTML('config/assets/coldchaindemo.html');
+                } else if (currentTab === 1) {
+                    odkTables.launchHTML('config/assets/rowlevelaccessdemo.html');
+                } else {
+                    console.log('trouble, unrecognized tab');
+                }
+            });
+}
+
+function updateForTab(tab) {
+    var fileUri;
+    var tabItem;
+    var descriptionDiv = $('#description');
+    // Remove all the active-tab classes.
+    $('.tab-item').removeClass('active');
+    // Now add the current tab to active and update the description.
+    if (tab === 0) {
+        // Cold Chain Demo
+        fileUri = odkCommon.getFileAsUrl('config/assets/img/hallway.jpg');
+        descriptionDiv.text('Cold Chain Management');
+        descriptionDiv.attr('class','description-text-white');
+        tabItem = $('#coldChainTab');
+    } else if (tab === 1) {
+        // Weather study
+        fileUri = odkCommon.getFileAsUrl('config/assets/img/20160902_sky.JPG');
+        descriptionDiv.text('Weather');
+        descriptionDiv.attr('class','description-text-white');
+        tabItem = $('#geoWeatherTab');
+    } else {
+        console.log('unrecognized tab index: ' + tab);
+    }
+    //$('#appImage').attr('src', fileUri);
+    $('body').css('background-image', 'url(' + fileUri + ')');
+    // Make the tab highlighted as active.
+    tabItem.addClass('active');
+}
