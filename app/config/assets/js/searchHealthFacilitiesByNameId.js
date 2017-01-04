5712709cbbce4cbfac3fe1ab3adae306850b64ae
clarice <clarlars@gmail.com>
2017-01-03 16:13:51 -0800

    Add facilities names and ids to dataList for search text box

--- app/config/assets/js/searchHealthFacilitiesByNameId.js
+++ app/config/assets/js/searchHealthFacilitiesByNameId.js
@@ -18,6 +18,9 @@ function display() {
     // Set the background to be a picture.
     body.css('background-image', 'url(img/hallway.jpg)');
 
+    // Query all the facilities to get the names and ids
+    odkData.query(tableId, null, null, null, null, null, null, null, null, false, cbFacilitySuccess, cbFacilityFailure);
+
     var searchFacilitiesButton = $('#search-facilities-by-name-id');
     searchFacilitiesButton.on(
         'click',
@@ -39,8 +42,28 @@ function display() {
                 selectionArgs.push(searchText);
             }
 
-            odkTables.openTableToListView('health_facility', selection, selectionArgs, 'config/tables/health_facility/html/health_facility_list.html');
+            odkTables.openTableToListView(tableId, selection, selectionArgs, 'config/tables/health_facility/html/health_facility_list.html');
         }
     );
+}
 
+function cbFacilitySuccess(result) {
+    // Populate data list options with names and ids
+    var facilityResultSet = result;
+
+    for (var i = 0; i < facilityResultSet.getCount(); i++) {
+        var fName = facilityResultSet.getData(i, 'facility_name');
+        var fId = facilityResultSet.getData(i, 'facility_id');
+
+        // Add both of these to options list
+        var optionName = $('<option value="'+fName+'"></option>');
+        $('#facilities').append(optionName);
+
+        var optionId = $('<option value="'+fId+'"></option>');
+        $('#facilities').append(optionId);
+    }
+}
+
+function cbFacilityFailure(error) {
+    console.log('cbFacilityFailure: query for facility name failed with error: ' + error);
 }
