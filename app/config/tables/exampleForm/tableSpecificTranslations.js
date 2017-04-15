df09a9eb34b89d565e50155b43a94e7cc067bb05
Mitch Sundt <mitchellsundt@gmail.com>
2017-04-14 17:03:31 -0700

    fix: table specific translation failure. Example form changed to illustrate use of table specific translations.

--- app/config/tables/exampleForm/tableSpecificTranslations.js
+++ app/config/tables/exampleForm/tableSpecificTranslations.js
@@ -1,9 +1,30 @@
 window.odkTableSpecificTranslations = {
-  "_tokens": {},
+  "_tokens": {
+    "first_prompt": {
+      "string_token": "first_prompt",
+      "text": {
+        "default": "Enter an initial rating (1-10) for this survey",
+        "hindi": "HINDI Enter an initial rating (1-10) for this survey"
+      },
+      "_row_num": 2
+    }
+  },
   "_locales": {
     "setting_name": "_locales",
     "_row_num": 4,
     "value": [
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "english": "Hindi",
+              "hindi": "Hindi (as hindi name)"
+            }
+          }
+        },
+        "_row_num": 6,
+        "name": "hindi"
+      },
       {
         "locale": {
           "text": "default"
@@ -15,6 +36,6 @@ window.odkTableSpecificTranslations = {
   "_default_locale": {
     "setting_name": "_default_locale",
     "_row_num": 4,
-    "value": "default"
+    "value": "hindi"
   }
 }
