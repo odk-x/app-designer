dd2753ab82922338e40f53ce1f01dc86845c4741
Ori Levari <olevari@uw.edu>
2017-05-02 17:48:54 -0700

    a little over half of translations implemented.

--- app/config/assets/commonTranslations.js
+++ app/config/assets/commonTranslations.js
@@ -4,9 +4,153 @@ window.odkCommonTranslations = {
       "string_token": "select_user",
       "text": {
         "default": "Please Select User",
-        "spanish": "por favor"
+        "spanish": "Seleccione usuario"
       },
       "_row_num": 2
+    },
+    "main_title": {
+      "string_token": "main_title",
+      "text": {
+        "default": "Disaster Relief",
+        "spanish": "Alivio de Desastres"
+      },
+      "_row_num": 3
+    },
+    "registration_path": {
+      "string_token": "registration_path",
+      "text": {
+        "default": "Registration",
+        "spanish": "Registro"
+      },
+      "_row_num": 4
+    },
+    "delivery_path": {
+      "string_token": "delivery_path",
+      "text": {
+        "default": "Delivery",
+        "spanish": "Entrega"
+      },
+      "_row_num": 5
+    },
+    "data_path": {
+      "string_token": "data_path",
+      "text": {
+        "default": "View Data",
+        "spanish": "Ver Datos"
+      },
+      "_row_num": 6
+    },
+    "override_path": {
+      "string_token": "override_path",
+      "text": {
+        "default": "Administrator Overrides",
+        "spanish": "Anulación del Administrador"
+      },
+      "_row_num": 7
+    },
+    "registration_title": {
+      "string_token": "registration_title",
+      "text": {
+        "default": "Register Beneficiary Code",
+        "spanish": "Registro del Código de Beneficiario"
+      },
+      "_row_num": 8
+    },
+    "delivery_title": {
+      "string_token": "delivery_title",
+      "text": {
+        "default": "Verify Beneficiary Code",
+        "spanish": "Verificar el Código del Beneficiario"
+      },
+      "_row_num": 9
+    },
+    "disabled_beneficiary_notification": {
+      "string_token": "disabled_beneficiary_notification",
+      "text": {
+        "default": "This beneficiary is disabled",
+        "spanish": "Este beneficiario está deshabilitado"
+      },
+      "_row_num": 10
+    },
+    "missing_beneficiary_notification": {
+      "string_token": "missing_beneficiary_notification",
+      "text": {
+        "default": "Scanned code not found in system",
+        "spanish": "Código del beneficiario no encontrado en el sistema"
+      },
+      "_row_num": 11
+    },
+    "barcode_available": {
+      "string_token": "barcode_available",
+      "text": {
+        "default": "Barcode Available",
+        "spanish": "Código disponible"
+      },
+      "_row_num": 12
+    },
+    "voucher_detected": {
+      "string_token": "voucher_detected",
+      "text": {
+        "default": "Voucher Barcode Detected",
+        "spanish": "Código del vale detectado"
+      },
+      "_row_num": 13
+    },
+    "barcode_unavailable": {
+      "string_token": "barcode_unavailable",
+      "text": {
+        "default": "Barcode Unavailable",
+        "spanish": "Beneficiario no disponible"
+      },
+      "_row_num": 14
+    },
+    "already_qualifies_override": {
+      "string_token": "already_qualifies_override",
+      "text": {
+        "default": "Scanned beneficiary already qualifies for this authorization",
+        "spanish": "El beneficiario escaneado ya califica para esta autorización"
+      },
+      "_row_num": 15
+    },
+    "eligible_override": {
+      "string_token": "eligible_override",
+      "text": {
+        "default": "Beneficiary eligible for override",
+        "spanish": "Beneficiario elegible para derecho"
+      },
+      "_row_num": 16
+    },
+    "override_creation_success": {
+      "string_token": "override_creation_success",
+      "text": {
+        "default": "Override Successfully Created",
+        "spanish": "Derecho creada correctamente"
+      },
+      "_row_num": 17
+    },
+    "view_authorization_details": {
+      "string_token": "view_authorization_details",
+      "text": {
+        "default": "View Authorization Details",
+        "spanish": "Ver Detalles de Autorización"
+      },
+      "_row_num": 18
+    },
+    "scan_barcode": {
+      "string_token": "scan_barcode",
+      "text": {
+        "default": "Scan Barcode",
+        "spanish": "Escanear Código"
+      },
+      "_row_num": 19
+    },
+    "enter": {
+      "string_token": "enter",
+      "text": {
+        "default": "Enter",
+        "spanish": "Entrar"
+      },
+      "_row_num": 20
     }
   },
   "_locales": {
@@ -19,7 +163,8 @@ window.odkCommonTranslations = {
             "text": {
               "english": "English",
               "spanish": "Ingles",
-              "hindi": "English (as hindi name)"
+              "hindi": "English (as hindi name)",
+              "default": "English"
             }
           }
         },
@@ -32,7 +177,8 @@ window.odkCommonTranslations = {
             "text": {
               "english": "Hindi",
               "spanish": "Hindi",
-              "hindi": "Hindi (as hindi name)"
+              "hindi": "Hindi (as hindi name)",
+              "default": "Hindi"
             }
           }
         },
@@ -45,7 +191,8 @@ window.odkCommonTranslations = {
             "text": {
               "english": "Spanish",
               "spanish": "Espanol",
-              "hindi": "Espanol"
+              "hindi": "Espanol",
+              "default": "Spanish"
             }
           }
         },
--- app/config/assets/framework/frameworkTranslations.js
+++ app/config/assets/framework/frameworkTranslations.js
@@ -390,55 +390,5 @@ window.odkFrameworkTranslations = {
       },
       "_row_num": 57
     }
-  },
-  "_locales": {
-    "setting_name": "_locales",
-    "_row_num": 5,
-    "value": [
-      {
-        "display": {
-          "locale": {
-            "text": {
-              "english": "English",
-              "spanish": "Ingles",
-              "hindi": "English (as hindi name)"
-            }
-          }
-        },
-        "_row_num": 7,
-        "name": "english"
-      },
-      {
-        "display": {
-          "locale": {
-            "text": {
-              "english": "Hindi",
-              "spanish": "Hindi",
-              "hindi": "Hindi (as hindi name)"
-            }
-          }
-        },
-        "_row_num": 8,
-        "name": "hindi"
-      },
-      {
-        "display": {
-          "locale": {
-            "text": {
-              "english": "Spanish",
-              "spanish": "Espanol",
-              "hindi": "Espanol"
-            }
-          }
-        },
-        "_row_num": 9,
-        "name": "spanish"
-      }
-    ]
-  },
-  "_default_locale": {
-    "setting_name": "_default_locale",
-    "_row_num": 5,
-    "value": "english"
   }
 }
