70295a9eba7c30de48817a70aac23f942a70a605
Jeff Beorse <jbeorse@cs.washington.edu>
2017-05-11 19:25:47 -0700

    Fix erroneous overwrites

--- app/config/assets/commonDefinitions.js
+++ app/config/assets/commonDefinitions.js
@@ -1,5 +1,389 @@
 window.odkCommonDefinitions = {
-  "_tokens": {},
+  "_tokens": {
+    "select_user": {
+      "string_token": "select_user",
+      "text": {
+        "default": "Please Select User",
+        "spanish": "Seleccione usuario"
+      },
+      "_row_num": 2
+    },
+    "main_title": {
+      "string_token": "main_title",
+      "text": {
+        "default": "Disaster Relief",
+        "spanish": "Alivio de Desastres"
+      },
+      "_row_num": 3
+    },
+    "registration_path": {
+      "string_token": "registration_path",
+      "text": {
+        "default": "Registration",
+        "spanish": "Registro"
+      },
+      "_row_num": 4
+    },
+    "delivery_path": {
+      "string_token": "delivery_path",
+      "text": {
+        "default": "Delivery",
+        "spanish": "Entrega"
+      },
+      "_row_num": 5
+    },
+    "data_path": {
+      "string_token": "data_path",
+      "text": {
+        "default": "View Data",
+        "spanish": "Ver Datos"
+      },
+      "_row_num": 6
+    },
+    "override_path": {
+      "string_token": "override_path",
+      "text": {
+        "default": "Administrator Overrides",
+        "spanish": "Anulación del Administrador"
+      },
+      "_row_num": 7
+    },
+    "registration_title": {
+      "string_token": "registration_title",
+      "text": {
+        "default": "Register Beneficiary Code",
+        "spanish": "Registro del Código de Beneficiario"
+      },
+      "_row_num": 8
+    },
+    "delivery_title": {
+      "string_token": "delivery_title",
+      "text": {
+        "default": "Verify Beneficiary Code",
+        "spanish": "Verificar el Código del Beneficiario"
+      },
+      "_row_num": 9
+    },
+    "disabled_beneficiary_notification": {
+      "string_token": "disabled_beneficiary_notification",
+      "text": {
+        "default": "This beneficiary is disabled",
+        "spanish": "Este beneficiario está deshabilitado"
+      },
+      "_row_num": 10
+    },
+    "missing_beneficiary_notification": {
+      "string_token": "missing_beneficiary_notification",
+      "text": {
+        "default": "Scanned code not found in system",
+        "spanish": "Código del beneficiario no encontrado en el sistema"
+      },
+      "_row_num": 11
+    },
+    "barcode_available": {
+      "string_token": "barcode_available",
+      "text": {
+        "default": "Barcode Available",
+        "spanish": "Código disponible"
+      },
+      "_row_num": 12
+    },
+    "voucher_detected": {
+      "string_token": "voucher_detected",
+      "text": {
+        "default": "Voucher Barcode Detected",
+        "spanish": "Código del vale detectado"
+      },
+      "_row_num": 13
+    },
+    "barcode_unavailable": {
+      "string_token": "barcode_unavailable",
+      "text": {
+        "default": "Barcode Unavailable",
+        "spanish": "Beneficiario no disponible"
+      },
+      "_row_num": 14
+    },
+    "already_qualifies_override": {
+      "string_token": "already_qualifies_override",
+      "text": {
+        "default": "Scanned beneficiary already qualifies for this authorization",
+        "spanish": "El beneficiario escaneado ya califica para esta autorización"
+      },
+      "_row_num": 15
+    },
+    "eligible_override": {
+      "string_token": "eligible_override",
+      "text": {
+        "default": "Beneficiary eligible for override",
+        "spanish": "Beneficiario elegible para derecho"
+      },
+      "_row_num": 16
+    },
+    "override_creation_success": {
+      "string_token": "override_creation_success",
+      "text": {
+        "default": "Override Successfully Created",
+        "spanish": "Derecho creada correctamente"
+      },
+      "_row_num": 17
+    },
+    "view_authorization_details": {
+      "string_token": "view_authorization_details",
+      "text": {
+        "default": "View Authorization Details",
+        "spanish": "Ver Detalles de Autorización"
+      },
+      "_row_num": 18
+    },
+    "scan_barcode": {
+      "string_token": "scan_barcode",
+      "text": {
+        "default": "Scan Barcode",
+        "spanish": "Escanear Código"
+      },
+      "_row_num": 19
+    },
+    "enter": {
+      "string_token": "enter",
+      "text": {
+        "default": "Enter",
+        "spanish": "Entrar"
+      },
+      "_row_num": 20
+    },
+    "no_active_beneficiary": {
+      "string_token": "no_active_beneficiary",
+      "text": {
+        "default": "No Active Beneficiary Detected"
+      },
+      "_row_num": 21
+    },
+    "no_disabled_beneficiary": {
+      "string_token": "no_disabled_beneficiary",
+      "text": {
+        "default": "No Disabled Beneficiary Detected"
+      },
+      "_row_num": 22
+    },
+    "enter_beneficiary_code": {
+      "string_token": "enter_beneficiary_code",
+      "text": {
+        "default": "Please Enter Beneficiary Code"
+      },
+      "_row_num": 23
+    },
+    "active_beneficiaries_title": {
+      "string_token": "active_beneficiaries_title",
+      "text": {
+        "default": "Active Beneficiaries"
+      },
+      "_row_num": 24
+    },
+    "disabled_beneficiaries_title": {
+      "string_token": "disabled_beneficiaries_title",
+      "text": {
+        "default": "Disabled Beneficiaries"
+      },
+      "_row_num": 25
+    },
+    "beneficiary_data_title": {
+      "string_token": "beneficiary_data_title",
+      "text": {
+        "default": "Beneficiary Data"
+      },
+      "_row_num": 26
+    },
+    "delivery_data_title": {
+      "string_token": "delivery_data_title",
+      "text": {
+        "default": "Delivery Data"
+      },
+      "_row_num": 27
+    },
+    "beneficiary_lists": {
+      "string_token": "beneficiary_lists",
+      "text": {
+        "default": "Beneficiary Lists"
+      },
+      "_row_num": 28
+    },
+    "view_all_deliveries": {
+      "string_token": "view_all_deliveries",
+      "text": {
+        "default": "View All Deliveries"
+      },
+      "_row_num": 29
+    },
+    "advanced_search": {
+      "string_token": "advanced_search",
+      "text": {
+        "default": "Advanced Search"
+      },
+      "_row_num": 30
+    },
+    "data_categories": {
+      "string_token": "data_categories",
+      "text": {
+        "default": "Data Categories"
+      },
+      "_row_num": 31
+    },
+    "view_beneficiary_data": {
+      "string_token": "view_beneficiary_data",
+      "text": {
+        "default": "View Beneficiary Data"
+      },
+      "_row_num": 32
+    },
+    "view_delivery_data": {
+      "string_token": "view_delivery_data",
+      "text": {
+        "default": "View Delivery Data"
+      },
+      "_row_num": 33
+    },
+    "enable_beneficiary": {
+      "string_token": "enable_beneficiary",
+      "text": {
+        "default": "Enable Beneficiary"
+      },
+      "_row_num": 34
+    },
+    "disable_beneficiary": {
+      "string_token": "disable_beneficiary",
+      "text": {
+        "default": "Disable Beneficiary"
+      },
+      "_row_num": 35
+    },
+    "choose_method": {
+      "string_token": "choose_method",
+      "text": {
+        "default": "Choose Method"
+      },
+      "_row_num": 36
+    },
+    "override_options": {
+      "string_token": "override_options",
+      "text": {
+        "default": "Override Options"
+      },
+      "_row_num": 37
+    },
+    "override_registration": {
+      "string_token": "override_registration",
+      "text": {
+        "default": "Override Registration"
+      },
+      "_row_num": 38
+    },
+    "override_entitlement": {
+      "string_token": "override_entitlement",
+      "text": {
+        "default": "Override Entitlement"
+      },
+      "_row_num": 39
+    },
+    "serach_beneficiaries": {
+      "string_token": "serach_beneficiaries",
+      "text": {
+        "default": "Search for Beneficiaries"
+      },
+      "_row_num": 40
+    },
+    "search_deliveries": {
+      "string_token": "search_deliveries",
+      "text": {
+        "default": "Search for Deliveries"
+      },
+      "_row_num": 41
+    },
+    "beneficiary": {
+      "string_token": "beneficiary",
+      "text": {
+        "default": "beneficiary"
+      },
+      "_row_num": 42
+    },
+    "beneficiaries": {
+      "string_token": "beneficiaries",
+      "text": {
+        "default": "beneficiaries"
+      },
+      "_row_num": 43
+    },
+    "delivery": {
+      "string_token": "delivery",
+      "text": {
+        "default": "delivery"
+      },
+      "_row_num": 44
+    },
+    "deliveries": {
+      "string_token": "deliveries",
+      "text": {
+        "default": "deliveries"
+      },
+      "_row_num": 45
+    },
+    "found": {
+      "string_token": "found",
+      "text": {
+        "default": "found"
+      },
+      "_row_num": 46
+    },
+    "invalid_search": {
+      "string_token": "invalid_search",
+      "text": {
+        "default": "Invalid Search"
+      },
+      "_row_num": 47
+    },
+    "authorization_name": {
+      "string_token": "authorization_name",
+      "text": {
+        "default": "Authorization Name"
+      },
+      "_row_num": 48
+    },
+    "authorization_id": {
+      "string_token": "authorization_id",
+      "text": {
+        "default": "Authorization ID"
+      },
+      "_row_num": 49
+    },
+    "item_pack_name": {
+      "string_token": "item_pack_name",
+      "text": {
+        "default": "Item Pack Name"
+      },
+      "_row_num": 50
+    },
+    "item_pack_description": {
+      "string_token": "item_pack_description",
+      "text": {
+        "default": "Item Pack Description"
+      },
+      "_row_num": 51
+    },
+    "item_pack_id": {
+      "string_token": "item_pack_id",
+      "text": {
+        "default": "Item Pack ID"
+      },
+      "_row_num": 52
+    },
+    "beneficiary_code": {
+      "string_token": "beneficiary_code",
+      "text": {
+        "default": "Beneficiary Code"
+      },
+      "_row_num": 53
+    }
+  },
   "_locales": {
     "setting_name": "_locales",
     "_row_num": 5,
@@ -9,6 +393,7 @@ window.odkCommonDefinitions = {
           "locale": {
             "text": {
               "english": "English",
+              "spanish": "Ingles",
               "hindi": "English (as hindi name)",
               "default": "English"
             }
@@ -22,6 +407,7 @@ window.odkCommonDefinitions = {
           "locale": {
             "text": {
               "english": "Hindi",
+              "spanish": "Hindi",
               "hindi": "Hindi (as hindi name)",
               "default": "Hindi"
             }
@@ -29,6 +415,20 @@ window.odkCommonDefinitions = {
         },
         "_row_num": 8,
         "name": "hindi"
+      },
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "english": "Spanish",
+              "spanish": "Espanol",
+              "hindi": "Espanol",
+              "default": "Spanish"
+            }
+          }
+        },
+        "_row_num": 9,
+        "name": "spanish"
       }
     ]
   },
