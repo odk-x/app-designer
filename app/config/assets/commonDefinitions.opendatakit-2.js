9408662666ae04d5aac1da974ba195d1701af2c4
Jeff Beorse <jbeorse@cs.washington.edu>
2017-05-11 18:05:36 -0700

    Update jquery version and head references

---
+++ app/config/assets/commonDefinitions.opendatakit-2.js
@@ -0,0 +1,22 @@
+window.odkCommonDefinitions = {
+  "_tokens": {},
+  "_locales": {
+    "setting_name": "_locales",
+    "_row_num": 5,
+    "value": [
+      {
+        "display": {
+          "locale": {
+            "text": "default"
+          }
+        },
+        "name": "default"
+      }
+    ]
+  },
+  "_default_locale": {
+    "setting_name": "_default_locale",
+    "_row_num": 5,
+    "value": "default"
+  }
+}
