783d7a89964d96adeb882a805c90994a2bab54a2
Niles Rogoff <me@niles.xyz>
2017-07-15 21:42:31 -0700

    Add framework

---
+++ app/config/assets/commonDefinitions.js
@@ -0,0 +1,36 @@
+window.odkCommonDefinitions = {
+  "_tokens": {},
+  "_locales": {
+    "setting_name": "_locales",
+    "value": [
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "english": "English",
+              "spanish": "Inglés",
+              "default": "English"
+            }
+          }
+        },
+        "name": "english"
+      },
+      {
+        "display": {
+          "locale": {
+            "text": {
+              "english": "Spanish",
+              "spanish": "Español",
+              "default": "Spanish"
+            }
+          }
+        },
+        "name": "spanish"
+      }
+    ]
+  },
+  "_default_locale": {
+    "setting_name": "_default_locale",
+    "value": "english"
+  }
+}
