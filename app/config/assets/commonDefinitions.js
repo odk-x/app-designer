bb81b0decd6140ed7f70b54926cf23aa7c1d72af
Niles Rogoff <me@niles.xyz>
2017-07-18 13:55:50 -0700

    Framework test

--- app/config/assets/commonDefinitions.js
+++ app/config/assets/commonDefinitions.js
@@ -2,6 +2,7 @@ window.odkCommonDefinitions = {
   "_tokens": {},
   "_locales": {
     "setting_name": "_locales",
+    "_row_num": 5,
     "value": [
       {
         "display": {
@@ -13,6 +14,7 @@ window.odkCommonDefinitions = {
             }
           }
         },
+        "_row_num": 7,
         "name": "english"
       },
       {
@@ -25,12 +27,14 @@ window.odkCommonDefinitions = {
             }
           }
         },
+        "_row_num": 8,
         "name": "es"
       }
     ]
   },
   "_default_locale": {
     "setting_name": "_default_locale",
+    "_row_num": 5,
     "value": "english"
   }
 }
