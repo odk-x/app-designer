fa026bdebbf1d5c377d3d09974d8cea76c0bc756
Niles Rogoff <me@niles.xyz>
2017-07-24 20:50:46 -0700

    cleanup

--- app/config/assets/graph.js
+++ app/config/assets/graph.js
@@ -64,8 +64,8 @@ var ol = function ol() {
 }
 
 // If we need more than this the graph is going to look ugly anyways
-// Colors are Oxley, Serenade, Chilean Fire, Vulcan, Zest, Froly, Havelock Blue, Firebrick and Purple
-var all_colors = ["#85ac85", "#ffebd7", "#993300", "#37393d", "#e58755", "#ff8080", "#4891d9", "#cc2e2d", "#9900ff"]
+// Colors are Oxley, Serenade, Chilean Fire, Vulcan, Zest, Froly, Havelock Blue, Firebrick, Purple and Regal Blue
+var all_colors = ["#85ac85", "#ffebd7", "#993300", "#37393d", "#e58755", "#ff8080", "#4891d9", "#cc2e2d", "#9900ff", "#1f4864"]
 var current_color_idx = 0;
 var getCorner = function getCorner(center_x, center_y, x, y) {
 	var ret_x = 0;
