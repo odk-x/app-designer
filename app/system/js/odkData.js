e5f9f4a50a6271a777aed4f52a70a604ea52608c
Niles Rogoff <me@niles.xyz>
2017-07-25 11:35:53 -0700

    Revert "Merge pull request #36 from mitchellsundt/development"
    This reverts commit cacecb63cabc294f58c86999d7ca882d7da6464f, reversing
    changes made to ac75224f4a65ed3fecbb63e14756771ec0abd817.

--- app/system/js/odkData.js
+++ app/system/js/odkData.js
@@ -1051,7 +1051,7 @@ window.odkData = {
                 if ( ref === null || ref === undefined ) {
                     return retVal;
                 }
-                var ref = ref[choiceDataValue];
+                var ref = ref.value[choiceDataValue];
                 if ( ref === null || ref === undefined ) {
                     return retVal;
                 }
