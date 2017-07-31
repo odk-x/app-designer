99a003816b361e412620dcd43fe7f44657f0a27c
Niles Rogoff <me@niles.xyz>
2017-07-31 16:31:44 -0700

    More hope study

---
+++ app/config/tables/maleClients/js/maleClients_detail.js
@@ -0,0 +1,10 @@
+/**
+ * The file for displaying a detail view.
+ */
+'use strict';
+ 
+function display() {
+    // Perform your modification of the HTML page here and call display() in
+    // the body of your .html file.
+}
+
