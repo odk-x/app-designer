863bca0b179b231e5657ff4f37092d4a40b90af2
Niles Rogoff <me@niles.xyz>
2017-08-12 14:13:13 -0700

    Extract user javascript properly

--- app/config/assets/userjs/f10f3090-460e-1fa3-785fd79f7552.js
+++
@@ -1,8 +0,0 @@
-
-	var tabs = [
-		{"title": "General", "file": "general.html"},
-		{"title": "Tea", "file": "th_index.html"},
-		{"title": "Selects", "file": "selects_index.html"},
-		{"title": "Plot", "file": "plot_index.html"},
-		//["Hope", "hope_index.html"]
-	]
