f6267e46ee4d7a070f3b44572bc41e07494f9293
clarlars <clarlars@gmail.com>
2017-09-13 15:44:23 -0700

    Add search functionality and clean up with promises

--- app/config/tables/refrigerators/js/refrigerators_list_by_district.js
+++ app/config/tables/refrigerators/js/refrigerators_list_by_district.js
@@ -1,61 +1,110 @@
 /**
- * This is the file that will create the list view for the refrigerator inventory table.
+ * This is the file that will create the list view for the table.
  */
 /* global $, odkCommon, odkData, odkTables, util */
 'use strict';
 
-var refrigeratorsResultSet = {};
-var typeData = {};
-var typeIdMap = {};
 var idxStart = -1;
 var limit = 20;
 var offset = 0;
 var rowCount = 0;
+var tableId = 'refrigerators';
 
 var districtQuery = 'SELECT * FROM refrigerators ' + 
     'JOIN health_facility ON refrigerators.facility_row_id = health_facility._id ' +
     'JOIN refrigerator_types ON refrigerators.model_row_id = refrigerator_types._id ' +
     'WHERE health_facility.admin_region = ?';
 
+var cntDistrictQuery = 'SELECT COUNT(*) AS cnt FROM (' +
+    districtQuery + ')';
+
 var districtSearchQuery = 'SELECT * FROM refrigerators ' + 
     'JOIN health_facility ON refrigerators.facility_row_id = health_facility._id ' +
     'JOIN refrigerator_types ON refrigerators.model_row_id = refrigerator_types._id ' +
-    'WHERE (health_facility.admin_region = ? AND (facility_name LIKE ? OR facility_id LIKE ?))';
-
-var cntDistrictQuery = 'SELECT COUNT(*) AS cnt FROM (' +
-    districtQuery + ')';
+    'WHERE (health_facility.admin_region = ? AND (facility_name LIKE ? OR facility_id LIKE ? OR ' + 
+    'tracking_id LIKE ? OR refrigerator_id LIKE ?))';
 
 var cntDistrictSearchQuery = 'SELECT COUNT(*) AS cnt FROM (' +
     districtSearchQuery + ')';
 
+var queryToRun = null;
+var queryToRunParams = null;
+var cntQueryToRun = null;
 
-
-/* Called when page loads to display things (Nothing to edit here) */
-function resumeFn(fIdxStart) {
-    var dist = util.getQueryParameter(util.leafRegion);
-    if (dist === null) {
-        console.log('No valid district passed in - nothing to display');
-        return;
+function processPromises(cntResultSet, resultSet) {
+    // Set the text for the number of rows
+    if (cntResultSet.getCount() > 0) {
+        rowCount = cntResultSet.getData(0, 'cnt');
+    } else {
+        rowCount = 0;
     }
 
-     $('#header').text(dist);
+    if (rowCount == 0) {
+        offset = 0;
+    }
 
-    // Use promise here
-    odkData.arbitraryQuery('refrigerators', 
-        cntDistrictQuery, [dist], limit, offset, cbCntSuccess, cbCntFailure);
+    // Display the results in the list
+    updateNavButtons();
+    updateNavText();
+    clearRows();
 
-    odkData.arbitraryQuery('refrigerators', 
-        districtQuery, [dist], limit, offset, cbSuccess, cbFailure);
+    if (resultSet.getCount() === 0) {
+        console.log('No ' + util.formatDisplayText(tableId));
+        var note = $('<li>');
+        note.attr('class', 'note');
+        note.text('No ' + util.formatDisplayText(tableId));
+        $('#list').append(note);
 
+    } else {
+        displayGroup(resultSet);
+    }
+}
+
+function resumeFn(fIdxStart) {
     idxStart = fIdxStart;
     console.log('resumeFn called. idxStart: ' + idxStart);
 
     if (idxStart === 0) {
+        rowCount = 0;
+        offset = 0;
+        limit = parseInt($('#limitDropdown option:selected').text());
 
+        // Init display
+        var dist = util.getQueryParameter(util.leafRegion);
+        if (dist === null) {
+            console.log('No valid district passed in - nothing to display');
+            return;
+        }
+
+        $('#header').text(dist);
+
+        cntQueryToRun = cntDistrictQuery;
+        queryToRun = districtQuery;
+        queryToRunParams = [dist];
+    }
+
+    var cntDistrictQueryPromise = new Promise(function(resolve, reject) {
+        odkData.arbitraryQuery(tableId, 
+            cntQueryToRun, queryToRunParams, null, null, resolve, reject);
+    });
+
+    var districtQueryPromise = new Promise(function(resolve, reject)  {
+        odkData.arbitraryQuery(tableId, 
+            queryToRun, queryToRunParams, limit, offset, resolve, reject);
+        
+    });
+
+    Promise.all([cntDistrictQueryPromise, districtQueryPromise]).then(function(resultArray) {
+        processPromises(resultArray[0], resultArray[1]);
+
+    }, function(err) {
+        console.log('district promises failed with error: ' +  err);
+    });
+
+    if (idxStart === 0) {
         // We're also going to add a click listener on the wrapper ul that will
         // handle all of the clicks on its children.
         $('#list').click(function(e) {
-            var tableId = refrigeratorsResultSet.getTableId();
             // wrap up the object so we can call closest()
             var jqueryObject = $(e.target);
             // we want the closest thing with class item_space, which we
@@ -74,24 +123,16 @@ function resumeFn(fIdxStart) {
     }
 }
 
-function displayGroup(idxStart) {
-    console.log('display group called. idxStart: ' + idxStart);
-
-    var offsetDisplay = offset + 1;
-    $('#navTextOffset').text(offsetDisplay);
-   
-    var limitVal = (offset + limit > rowCount) ? rowCount : offset + limit;
-    $('#navTextLimit').text(limitVal);
-
+function displayGroup(resultSet) {
     /* Number of rows displayed per 'chunk' - can modify this value */
-    for (var i = 0; i < refrigeratorsResultSet.getCount(); i++) {
+    for (var i = 0; i < resultSet.getCount(); i++) {
 
         /* Creates the item space */
         var item = $('<li>');
-        var catalogID = refrigeratorsResultSet.getData(i, 'catalog_id');
-        item.attr('rowId', refrigeratorsResultSet.getRowId(i));
+        var catalogID = resultSet.getData(i, 'catalog_id');
+        item.attr('rowId', resultSet.getRowId(i));
         item.attr('class', 'item_space');
-        item.text('Refrigerator ' + refrigeratorsResultSet.getData(i, 'tracking_id') +
+        item.text('Refrigerator ' + resultSet.getData(i, 'tracking_id') +
             ' | ' + catalogID);
 
         /* Creates arrow icon (Nothing to edit here) */
@@ -101,7 +142,7 @@ function displayGroup(idxStart) {
         item.append(chevron);
 
         var field1 = $('<li>');
-        var facilityName = refrigeratorsResultSet.getData(i, 'facility_name');
+        var facilityName = resultSet.getData(i, 'facility_name');
         field1.attr('class', 'detail');
         field1.text('Healthcare Facility: ' + facilityName);
         item.append(field1);
@@ -113,57 +154,24 @@ function displayGroup(idxStart) {
         borderDiv.addClass('divider');
         $('#list').append(borderDiv);
     }
-
-    if (i < refrigeratorsResultSet.getCount()) {
-        setTimeout(resumeFn, 0, i);
-    }
-
-}
-
-function cbCntSuccess(result) {
-
-    $('#navTextCnt').text(rowCount);
-
-    refrigeratorsResultSet = result;
-
-    if (refrigeratorsResultSet.getCount() > 0) {
-        rowCount = refrigeratorsResultSet.getData(0, 'cnt');
-        $('#navTextCnt').text(rowCount);
-    } 
-}
-
-function cbCntFailure(error) {
-
-    console.log('cbCntFaillure: failed with error: ' + error);
-
-}
-
-function cbSuccess(result) {
-
-    refrigeratorsResultSet = result;
-    updateNavButtons();
-
-    if (refrigeratorsResultSet.getCount() === 0) {
-        console.log('No Refrigerators');
-        var note = $('<li>');
-        note.attr('class', 'note');
-        note.text('No Refrigerators');
-        $('#list').append(note);
-
-    } else {
-
-        displayGroup(idxStart);
-    }
-}
-
-function cbFailure(error) {
-
-    console.log('getViewData failed with error: ' + error);
-
 }
 
 function clearRows() {
   $('#list').empty();
+}
+
+function updateNavText() {
+    $('#navTextCnt').text(rowCount);
+    if (rowCount <= 0) {
+        $('#navTextOffset').text(0);
+        $('#navTextLimit').text(0);
+    } else {
+        var offsetDisplay = offset + 1;
+        $('#navTextOffset').text(offsetDisplay);
+
+        var limitVal = (offset + limit >= rowCount) ? rowCount : offset + limit;
+        $('#navTextLimit').text(limitVal);
+    }
 }
 
 function updateNavButtons() {
@@ -173,85 +181,83 @@ function updateNavButtons() {
     $('#prevButton').prop('disabled',false);
   }
 
-  if (offset + limit > rowCount) {
+  if (offset + limit >= rowCount) {
     $('#nextButton').prop('disabled',true);  
   } else {
      $('#nextButton').prop('disabled',false);  
   }
 }
 
-/**
- * Chunk the displays for larger tables
- */
 function prevResults() {
-  offset -= limit;
-  if (offset < 0) {
-    offset = 0;
-  }
+    offset -= limit;
+    if (offset < 0) {
+        offset = 0;
+    }
 
-  updateNavButtons();
+    updateNavButtons();
 
-  clearRows();
-  resumeFn(0);
+    clearRows();
+    resumeFn(1);
 }
 
 function nextResults() {
-  updateNavButtons();
+    updateNavButtons();
 
-  if (offset + limit > rowCount) {  
-    return;
-  }
-
-  offset += limit;
-
-  clearRows();
-  resumeFn(0);
-}
-
-function refrigeratorTypesCBSuccess(result) {
-
-    typeData = result;
-    console.log('refrigeratorTypesCBSuccess');
-    for (var i = 0; i < typeData.getCount(); i++) {
-        console.log('Row ID: ' + typeData.getRowId(i));
-        console.log('Catalog ID: ' + typeData.getData(i, 'catalog_id'));
-        typeIdMap[typeData.getRowId(i)] = typeData.getData(i, 'catalog_id');
+    if (offset + limit >= rowCount) {  
+        return;
     }
 
-    displayGroup(idxStart);
+    offset += limit;
 
+    clearRows();
+    resumeFn(1);
 }
 
-function refrigeratorTypesCBFailure(error) {
+function newLimit() {
 
-    console.log('refrigeratorTypesCBFailure: failed with error: ' + error);
+    limit = parseInt($('#limitDropdown option:selected').text());
 
-}
-
-function cbSearchSuccess (searchData) {
-    console.log('cbSearchSuccess data is' + searchData);
-    if(searchData.getCount() > 0) {
-        // open filtered list view if facility found
-        var rowId = searchData.getRowId(0);
-        odkTables.openTableToListView(
-                null, 
-                'refrigerators',
-                '_id = ?',
-                [rowId],
-                'config/tables/refrigerators/html/refrigerators_list.html');
-    } else {
-        document.getElementById('search').value = '';
-        document.getElementsByName('query')[0].placeholder='Refrigerator not found';
-    }
-}
-
-function cbSearchFailure(error) {
-    console.log('refrigerators_list cbSearchFailure: ' + error);
+    clearRows();
+    resumeFn(3);
 }
 
 function getSearchResults () {
-    var searchText = document.getElementById('search').value;
+    var dist = util.getQueryParameter(util.leafRegion);
+    if (dist === null) {
+        console.log('No valid district passed in - nothing to display');
+        return;
+    }
 
-    odkData.query('refrigerators', 'refrigerator_id = ?', [searchText],
-        null, null, null, null, null, null, true, cbSearchSuccess, cbSearchFailure);
+    var searchText = $('#search').val();
+
+    if (searchText !== null && searchText !== undefined &&
+        searchText.length !== 0) {
+        searchText = '%' + searchText + '%';
+        cntQueryToRun = cntDistrictSearchQuery;
+        queryToRun = districtSearchQuery;
+        queryToRunParams = [dist, searchText, searchText, searchText, searchText];
+        offset = 0;
+        resumeFn(5);
+    }
+    
+}
+
+function clearResults() {
+    var dist = util.getQueryParameter(util.leafRegion);
+    if (dist === null) {
+        console.log('No valid district passed in - nothing to display');
+        return;
+    }
+
+    var searchText = $('#search').val();
+
+    if (searchText === null || searchText === undefined ||
+        searchText.length === 0) {
+
+        cntQueryToRun = cntDistrictQuery;
+        queryToRun = districtQuery;
+        queryToRunParams = [dist];
+        offset = 0;
+        resumeFn(6);  
+    }  
 }
