00e84edb673cd74f348c97ca9b7dbb190c339676
Ori Levari <olevari@uw.edu>
2017-09-19 19:08:49 -0700

    Corrected reference to tables in the queries sheets of custom_beneficiaries and custom_individuals. Changed the required rows in the custom individuals form to only be custom_beneficiary_entity_row_id

---
+++ app/config/tables/custom_individuals/tableSpecificDefinitions.js
@@ -0,0 +1,34 @@
+window.odkTableSpecificDefinitions = {
+  "_tokens": {
+    "id_number": {
+      "string_token": "id_number",
+      "text": "ID Number",
+      "_row_num": 2
+    },
+    "id_type": {
+      "string_token": "id_type",
+      "text": "ID Type",
+      "_row_num": 3
+    },
+    "gender": {
+      "string_token": "gender",
+      "text": "Gender",
+      "_row_num": 4
+    },
+    "age": {
+      "string_token": "age",
+      "text": "Age",
+      "_row_num": 5
+    },
+    "beneficiary_code": {
+      "string_token": "beneficiary_code",
+      "text": "Beneficiary Code",
+      "_row_num": 6
+    },
+    "name": {
+      "string_token": "name",
+      "text": "Name",
+      "_row_num": 7
+    }
+  }
+}
