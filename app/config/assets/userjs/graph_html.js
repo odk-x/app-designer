c7dfd1a17215c90ea1961697142d818a3a553328
clarlars <clarlars@gmail.com>
2017-09-21 14:19:10 -0700

    Reconciliation commit

--- app/config/assets/userjs/graph_html.js
+++
@@ -1,9 +0,0 @@
-
-			window.show_value = false;
-			window.iframeOnly = false;
-			window.sort = false;
-			window.reverse = false;
-			// If we need more than this the graph is going to look ugly anyways
-			// Colors are Oxley, Serenade, Chilean Fire, Vulcan, Zest, Froly, Havelock Blue, Firebrick, Purple, Regal Blue, Whiskey Sour, Cafe Royale, Fun Blue, Picton Blue and Reef
-			window.all_colors = ["#85ac85", "#ffebd7", "#993300", "#37393d", "#e58755", "#ff8080", "#4891d9", "#cc2e2d", "#9900ff", "#1f4864", "#DB9863", "#6E4B2A", "#233C6F", "#438CB7", "#B8D891"]
-	var use_d3 = false;
