a5861f38e7a582a1fc4a470cb7a38ff77a437815
clarlars <clarlars@gmail.com>
2017-10-06 18:53:47 -0700

    Remove debug options and add in comments for more functionality

--- app/config/tables/refrigerators/js/refrigerators_service_list.js
+++ app/config/tables/refrigerators/js/refrigerators_service_list.js
@@ -4,6 +4,17 @@
 /* global $, odkCommon, odkData, odkTables, util, listViewLogic */
 'use strict';
 
+// TODO: A query like this would be necessary if we were going to 
+// get refrigerators needing service in a specific timeframe
+// var listQuery = 'SELECT * FROM refrigerators ' + 
+//     'JOIN health_facility ON refrigerators.facility_row_id = health_facility._id ' +
+//     'JOIN refrigerator_types ON refrigerators.model_row_id = refrigerator_types._id ' +
+//     'JOIN (SELECT refrigerator_id, MAX(date_serviced) AS most_recent_date_serviced ' +
+//     'FROM maintenance_logs GROUP BY refrigerator_id) ' +
+//     'AS recent_log ON refrigerators.refrigerator_id = recent_log.refrigerator_id WHERE ' + 
+//     '(refrigerators.maintenance_priority = ? OR refrigerators.maintenance_priority = ? OR ' +
+//     'refrigerators.maintenance_priority = ?) AND most_recent_date_serviced > ?';
+
 var listQuery = 'SELECT * FROM refrigerators ' + 
     'JOIN health_facility ON refrigerators.facility_row_id = health_facility._id ' +
     'JOIN refrigerator_types ON refrigerators.model_row_id = refrigerator_types._id ' +
@@ -16,8 +27,18 @@ var listQuery = 'SELECT * FROM refrigerators ' +
 var listQueryParams = ['high', 'medium', 'low'];
 var searchParams = '(facility_name LIKE ? OR facility_id LIKE ? OR tracking_id LIKE ? OR refrigerator_id LIKE ?)';
 
+function addMonths(date, months) {
+    date.setMonth(date.getMonth() + months);
+    return date;
+}
+
 function resumeFunc(state) {
     if (state === 'init') {
+        // TODO: Find out if we need data dependent query
+        // If so, get the current date
+        // var date6MonthsAgo = odkCommon.toOdkTimeStampFromDate(addMonths(new Date(), -6));
+        // listQueryParams.push(date6MonthsAgo);
+
         // set the parameters for the list view
         listViewLogic.setTableId('refrigerators');
         listViewLogic.setListQuery(listQuery);
