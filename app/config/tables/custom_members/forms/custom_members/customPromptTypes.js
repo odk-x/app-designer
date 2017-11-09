5033cdf90dce74dfa92a63f2a9250c9713795823
linl33 <linlee1997@gmail.com>
2017-11-09 12:24:36 -0800

    name change individual -> member

--- app/config/tables/custom_members/forms/custom_members/customPromptTypes.js
+++ app/config/tables/custom_members/forms/custom_members/customPromptTypes.js
@@ -10,7 +10,7 @@ var async_assign = promptTypes.base.extend({
     type: "async_assign",
     debug: false,
     valid: true,
-    templatePath: '../config/tables/custom_individuals/forms/custom_individuals/templates/async_assign.handlebars',
+    templatePath: '../config/tables/custom_members/forms/custom_members/templates/async_assign.handlebars',
     _cachedSelection: null,
     getLinkedTableId: function() {
         var queryDefn = opendatakit.getQueriesDefinition(this.values_list);
