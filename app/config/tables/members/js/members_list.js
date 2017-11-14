4bc7d4f518371fd002c867740cbf0c0b143f1f73
Ori Levari <olevari@uw.edu>
2017-11-13 16:34:35 -0800

    add members table

---
+++ app/config/tables/members/js/members_list.js
@@ -0,0 +1,124 @@
+'use strict';
+
+var idxStart = -1;
+var membersResultSet = {};
+var locale = odkCommon.getPreferredLocale();
+
+
+var registrationCBFailure = function(error) {
+
+    console.log('registrationMember_list registrationCBFailure: ' + error);
+};
+
+var firstLoad = function() {
+    return resumeFn(0);
+};
+
+/**
+ * Called when page loads to display things (Nothing to edit here)
+ */
+var resumeFn = function(fIdxStart) {
+    var renderPromise = new Promise( function(resolve, reject) {
+        odkData.getViewData(resolve, reject);
+    }).then( function(result) {
+        membersResultSet = result;
+
+        return displayGroup(idxStart);
+    });
+
+
+    idxStart = fIdxStart;
+    console.log('resumeFn called. idxStart: ' + idxStart);
+    if (idxStart === 0) {
+        // We're also going to add a click listener on the wrapper ul that will
+        // handle all of the clicks on its children.
+        $('#list').click(function(e) {
+            var tableId = membersResultSet.getTableId();
+            // We set the rowId while as the li id. However, we may have
+            // clicked on the li or anything in the li. Thus we need to get
+            // the original li, which we'll do with jQuery's closest()
+            // method. First, however, we need to wrap up the target
+            // element in a jquery object.
+            // wrap up the object so we can call closest()
+            var jqueryObject = $(e.target);
+            // we want the closest thing with class item_space, which we
+            // have set up to have the row id
+            var containingDiv = jqueryObject.closest('.item_space');
+            var rowId = containingDiv.attr('rowId');
+            console.log('clicked with rowId: ' + rowId);
+            // make sure we retrieved the rowId
+            if (rowId !== null && rowId !== undefined) {
+                // we'll pass null as the relative path to use the default file
+                odkTables.openDetailView(null, tableId, rowId,
+                    'config/tables/' + util.membersTable + '/html/' + util.membersTable + '_detail.html');
+            }
+        });
+    }
+
+    return renderPromise;
+};
+
+/**
+ * Displays the list view in chunks or groups. Number of list entries per chunk
+ * can be modified. The list view is designed so that each row in the table is
+ * represented as a list item. If you touch a particular list item, it will
+ * expand with more details (that you choose to include). Clicking on this
+ * expanded portion will take you to the more detailed view.
+ */
+var displayGroup = function(idxStart) {
+    console.log('displayGroup called. idxStart: ' + idxStart);
+
+    /* If the list comes back empty, inform the user */
+    if (membersResultSet.getCount() === 0) {
+        var errorText = $('#error');
+        errorText.show();
+        errorText.text('No beneficiaries found'); // TODO: Translate this
+    }
+
+    /* Number of rows displayed per 'chunk' - can modify this value */
+    var dbActions = [];
+    var chunk = 50;
+    for (var i = idxStart; i < idxStart + chunk; i++) {
+        if (i >= membersResultSet.getCount()) {
+            break;
+        }
+        /* Creates the item space */
+        // We're going to select the ul and then start adding things to it.
+        //var item = $('#list').append('<li>');
+        var item = $('<li>');
+        item.attr('rowId', membersResultSet.getRowId(i));
+        item.attr('class', 'item_space');
+        item.attr('id', membersResultSet.getData(i, '_id'));
+
+        setTimeout((function f(htmlItem, index) {
+            dbActions.push(new Promise( function(resolve, reject) {
+                odkData.query(util.getMemberCustomFormId(), '_id = ?', [membersResultSet.getData(index, 'custom_member_row_id')],
+                    null, null, null, null, null, null, true, resolve, reject);
+            }).then( function(customMemberResultSet) {
+                let first_last_name = customMemberResultSet.getData(0, 'first_last_name');
+                htmlItem.text('Name' + ": " + first_last_name);
+            }));
+        })(item, i), 0);
+
+        //TODO: fix
+
+        /* Creates arrow icon (Nothing to edit here) */
+        /*var chevron = $('<img>');
+        chevron.attr('src', odkCommon.getFileAsUrl('config/assets/img/little_arrow.png'));
+        chevron.attr('class', 'chevron');
+        item.append(chevron);*/
+
+        $('#list').append(item);
+
+        // don't append the last one to avoid the fencepost problem
+        var borderDiv = $('<div>');
+        borderDiv.addClass('divider');
+        $('#list').append(borderDiv);
+    }
+
+
+    if (i < membersResultSet.getCount()) {
+        setTimeout(resumeFn, 0, i);
+    }
+    return Promise.all(dbActions);
+};
---
+++ app/config/tables/members/tableSpecificDefinitions.js
@@ -0,0 +1,124 @@
+window.odkTableSpecificDefinitions = {
+  "_tokens": {
+    "beneficiary_entity_row_id": {
+      "string_token": "beneficiary_entity_row_id",
+      "text": "Beneficiary Entity Row ID",
+      "_row_num": 2
+    },
+    "beneficiary_entity_status": {
+      "string_token": "beneficiary_entity_status",
+      "text": "Beneficiary Entity Status",
+      "_row_num": 3
+    },
+    "date_created": {
+      "string_token": "date_created",
+      "text": "Date Created",
+      "_row_num": 4
+    },
+    "individual_id": {
+      "string_token": "individual_id",
+      "text": "Individual ID",
+      "_row_num": 5
+    },
+    "custom_individual_form_id": {
+      "string_token": "custom_individual_form_id",
+      "text": "Custom Individual Form ID",
+      "_row_num": 6
+    },
+    "custom_individual_row_id": {
+      "string_token": "custom_individual_row_id",
+      "text": "Custom Individual Row ID",
+      "_row_num": 7
+    },
+    "status": {
+      "string_token": "status",
+      "text": "Status",
+      "_row_num": 8
+    },
+    "status_reason": {
+      "string_token": "status_reason",
+      "text": "Status Reason",
+      "_row_num": 9
+    },
+    "custom_beneficiary_entity_row_id": {
+      "string_token": "custom_beneficiary_entity_row_id",
+      "text": "Custom Beneficiary Entity Row ID",
+      "_row_num": 10
+    },
+    "date_screened": {
+      "string_token": "date_screened",
+      "text": "Date Screened",
+      "_row_num": 11
+    },
+    "first_name": {
+      "string_token": "first_name",
+      "text": "First Name",
+      "_row_num": 12
+    },
+    "last_name": {
+      "string_token": "last_name",
+      "text": "Last Name",
+      "_row_num": 13
+    },
+    "first_last_name": {
+      "string_token": "first_last_name",
+      "text": "First and Last Name",
+      "_row_num": 14
+    },
+    "birth_date": {
+      "string_token": "birth_date",
+      "text": "Birth Date",
+      "_row_num": 15
+    },
+    "age": {
+      "string_token": "age",
+      "text": "Age",
+      "_row_num": 16
+    },
+    "gender": {
+      "string_token": "gender",
+      "text": "Gender",
+      "_row_num": 17
+    },
+    "id_type": {
+      "string_token": "id_type",
+      "text": "ID Type",
+      "_row_num": 18
+    },
+    "id_number": {
+      "string_token": "id_number",
+      "text": "ID Number",
+      "_row_num": 19
+    },
+    "beneficiary_code": {
+      "string_token": "beneficiary_code",
+      "text": "Beneficiary Code",
+      "_row_num": 20
+    },
+    "verify_user": {
+      "string_token": "verify_user",
+      "text": "Verify User",
+      "_row_num": 21
+    },
+    "is_head_of_household": {
+      "string_token": "is_head_of_household",
+      "text": "Is Head of Household",
+      "_row_num": 22
+    },
+    "picture": {
+      "string_token": "picture",
+      "text": "Picture",
+      "_row_num": 23
+    },
+    "email": {
+      "string_token": "email",
+      "text": "Email",
+      "_row_num": 24
+    },
+    "vulnerability": {
+      "string_token": "vulnerability",
+      "text": "Vulnerability",
+      "_row_num": 25
+    }
+  }
+}
