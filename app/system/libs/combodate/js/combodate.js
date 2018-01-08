dde7124ee0323ecb6229ba1d90d6aab186ca3036
Jeff Beorse <jeff@beorse.net>
2018-01-08 14:32:06 -0800

    Default to month numbers instead of English abbreviations

--- app/system/libs/combodate/js/combodate.js
+++ app/system/libs/combodate/js/combodate.js
@@ -510,7 +510,7 @@
          //in this format value stored in original input
         format: 'DD-MM-YYYY HH:mm',
         //in this format items in dropdowns are displayed
-        template: 'DD / MMM / YYYY   HH : mm',
+        template: 'DD / MM / YYYY   HH : mm',
         //initial value, can be `new Date()`
         value: null,
         minYear: new Date().getFullYear() - 100,
