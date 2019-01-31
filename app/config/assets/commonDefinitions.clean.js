892fdbd87fdedde505bc9c8432d1a47b0a6c7a95
khmermenten <randy_yiv@yahoo.com>
2019-01-30 21:17:55 -0800

    language codes

--- app/config/assets/commonDefinitions.clean.js
+++ app/config/assets/commonDefinitions.clean.js
@@ -1,40 +1,40 @@
 window.odkCommonDefinitions = {
-  "_tokens": {},
-  "_locales": {
-    "setting_name": "_locales",
-    "_row_num": 5,
-    "value": [
-      {
-        "display": {
-          "locale": {
-            "text": {
-              "english": "English",
-              "hindi": "English (as hindi name)",
-              "default": "English"
-            }
-          }
-        },
-        "_row_num": 7,
-        "name": "english"
-      },
-      {
-        "display": {
-          "locale": {
-            "text": {
-              "english": "Hindi",
-              "hindi": "Hindi (as hindi name)",
-              "default": "Hindi"
-            }
-          }
-        },
-        "_row_num": 8,
-        "name": "hindi"
-      }
-    ]
-  },
-  "_default_locale": {
-    "setting_name": "_default_locale",
-    "_row_num": 5,
-    "value": "english"
-  }
+	  "_tokens": {},
+	  "_locales": {
+		      "setting_name": "_locales",
+		      "_row_num": 5,
+		      "value": [
+			            {
+					            "display": {
+							              "locale": {
+									                  "text": {
+												                "en": "English (EN)",
+												                "hi": "English (as hindi name) (EN)",
+												                "default": "English (EN)"
+												              }
+									                }
+							            },
+					            "_row_num": 7,
+					            "name": "en"
+					          },
+			            {
+					            "display": {
+							              "locale": {
+									                  "text": {
+												                "en": "Hindi (HI)",
+												                "hi": "Hindi (as hindi name) (HI)",
+												                "default": "Hindi (HI)"
+												              }
+									                }
+							            },
+					            "_row_num": 8,
+					            "name": "hi"
+					          }
+			          ]
+		    },
+	  "_default_locale": {
+		      "setting_name": "_default_locale",
+		      "_row_num": 5,
+		      "value": "en"
+		    }
 }
--- app/config/assets/commonDefinitions.js
+++ app/config/assets/commonDefinitions.js
@@ -8,38 +8,38 @@ window.odkCommonDefinitions = {
         "display": {
           "locale": {
             "text": {
-              "english": "English",
-              "hindi": "English (as hindi name)",
-              "es": "Inglés",
-              "default": "English"
+              "en": "English (EN)",
+              "hi": "English (as hindi name) (EN)",
+              "es": "Inglés (EN)",
+              "default": "English (EN)"
             }
           }
         },
         "_row_num": 7,
-        "name": "english"
+        "name": "en"
       },
       {
         "display": {
           "locale": {
             "text": {
-              "english": "Hindi",
-              "hindi": "Hindi (as hindi name)",
-              "es": "Hindi",
-              "default": "Hindi"
+              "en": "Hindi (HI)",
+              "hi": "Hindi (as hindi name) (HI)",
+              "es": "Hindi (HI)",
+              "default": "Hindi (HI)"
             }
           }
         },
         "_row_num": 8,
-        "name": "hindi"
+        "name": "hi"
       },
       {
         "display": {
           "locale": {
             "text": {
-              "english": "Spanish",
-              "hindi": "Spanish (as hindi name)",
-              "es": "Español",
-              "default": "Spanish"
+              "en": "Spanish (ES)",
+              "hi": "Spanish (as hindi name) (ES)",
+              "es": "Español (ES)",
+              "default": "Spanish (ES)"
             }
           }
         },
@@ -51,6 +51,6 @@ window.odkCommonDefinitions = {
   "_default_locale": {
     "setting_name": "_default_locale",
     "_row_num": 5,
-    "value": "english"
+    "value": "en"
   }
 }
