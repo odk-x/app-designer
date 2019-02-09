c63ce494ec03d6d564ca5041cb50927f630cc9dc
linl33 <linlee1997@gmail.com>
2019-02-09 10:46:14 -0800

    Update js-xlsx to 0.14.1

--- xlsxconverter/js-xlsx/bits/98_exports.js
+++
@@ -1,7 +0,0 @@
-XLSX.parseZip = parse_zip;
-XLSX.read = readSync;
-XLSX.readFile = readFileSync;
-XLSX.write = writeSync;
-XLSX.writeFile = writeFileSync;
-XLSX.utils = utils;
-XLSX.SSF = SSF;
--- xlsxconverter/js-xlsx/odsbits/20_jsutils.js
+++
@@ -1,1 +0,0 @@
-var has_buf = (typeof Buffer !== 'undefined');
